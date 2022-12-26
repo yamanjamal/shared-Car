fc1e557db063db2ed64848aba95a7cf870027120
yamanjamal <yamanjamal09@gmail.com>
2022-12-26 14:35:29 +0200

    adding has roles to user model

--- src/app/Models/User.php
+++ src/app/Models/User.php
@@ -12,10 +12,11 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 use App\Models\Concerns\HasUUID;
+use Spatie\Permission\Traits\HasRoles;
 
 class User extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable, HasUUID;
+    use HasApiTokens, HasFactory, Notifiable, HasUUID, HasRoles;
 
     protected $fillable = ['name', 'last_name', 'username', 'email', 'password', 'phone', 'rate'];
 
